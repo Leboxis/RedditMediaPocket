0f219ccc0b3514cdb17dff4ce66d5d19ab0b9e58
Leboxis <Leboxis@users.noreply.github.com>
2026-09-21 14:52:50 +0200

    Fix CI: swift-tools 6.2 for v26 platforms, keep Swift 5 mode

--- Package.swift
+++ Package.swift
@@ -1,3 +1,3 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.2
 import PackageDescription
-let package = Package(name: "MediaCore", platforms: [.macOS(.v26), .iOS(.v26)], products: [.library(name: "MediaCore", targets: ["MediaCore"])], targets: [.target(name: "MediaCore"), .testTarget(name: "MediaCoreTests", dependencies: ["MediaCore"])])
+let package = Package(name: "MediaCore", platforms: [.macOS(.v26), .iOS(.v26)], products: [.library(name: "MediaCore", targets: ["MediaCore"])], targets: [.target(name: "MediaCore"), .testTarget(name: "MediaCoreTests", dependencies: ["MediaCore"])], swiftLanguageModes: [.v5])
